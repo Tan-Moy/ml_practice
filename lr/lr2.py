bb8dfea8d389566fdd5c56dc6fbba92a6044ce8c
Tan <tanmoy.pto@gmail.com>
2017-11-25 12:40:08 +0530

    linear regression model built

--- lr/lr2.py
+++ lr/lr2.py
@@ -1,116 +1,45 @@
-
-# coding: utf-8
-
-# In[191]:
-
-import os
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-# get_ipython().run_line_magic('matplotlib', 'inline')
+import numpy as np
+# from numpy import genfromtxt
+# this is your 'bad' data set from github
+my_data = np.genfromtxt('data.csv', delimiter=',')
 
+def computeCost(X, y, theta):
+    inner = np.power(((X @ theta.T) - y), 2) # @ means matrix multiplication
+    return np.sum(inner) / (2 * len(X))
 
-# In[207]:
-
-data = pd.read_csv('data2.csv',names=['X','Y'])
-data.head()
-
-
-# In[208]:
-
-data.describe()
-
-
-# In[209]:
-
-# data.plot(kind='scatter', x='X',y='Y',figsize=(12,8))
-
-
-# In[210]:
-
-data.insert(0,'Ones',1)
-data.head()
-
-
-# In[211]:
-
-#create the training and target vars
-cols = data.shape[1]
-X = data.iloc[:,0:cols-1]
-y = data.iloc[:,cols-1:cols]
-
-
-# In[212]:
-
-X = np.matrix(X.values)
-y = np.matrix(y.values)
-theta = np.matrix(np.array([0.0,0.0]))
-
-
-# In[213]:
-
-#print(theta)
-
-
-# In[214]:
-
-X.shape,theta.shape,y.shape
-
-
-# In[215]:
-
-def computeCost (X,y,theta):
-    inner = np.power(((X*theta.T)-y),2)
-    return np.sum(inner)/(2*len(X))
-
-
-# In[216]:
-
-computeCost(X,y,theta)
-
-
-# In[217]:
-
-#print(np.sum(np.multiply(2782,X[:,0])))
-
-
-# In[218]:
-
-def gradientDescent(X,y,theta,alpha,iters):
-    temp = np.matrix(np.zeros(theta.shape))
-    parameters = int(theta.ravel().shape[1])
-    cost = np.zeros(iters)
-    
+def gradientDescent(X, y, theta, alpha, iters):
     for i in range(iters):
-        error = (X * theta.T) - y
-        
-        for j in range(parameters):
-            term = np.multiply(error,X[:,j]) #differentiating the term inside of sigma in cost function gives this
-            temp[0,j] = theta[0,j] - ((alpha/len(X))*np.sum(term)) #from gradient descent formula
-        
-        theta = temp
-        cost[i] = computeCost(X,y,theta)
-        
-    return theta,cost
+        # you don't need the extra loop - this can be vectorize
+        # making it much faster and simpler
+        theta = theta - (alpha/len(X)) * np.sum((X @ theta.T - y) * X, axis=0)
+        cost = computeCost(X, y, theta)
+        # if i % 10 == 0: # just look at cost every ten loops for debugging
+        #     print(cost)
+    return (theta, cost)
 
-
-# In[223]:
-
-# initialize learning parameters 
-alpha = 0.01
+# notice small alpha value
+alpha = 0.0001
 iters = 1000
 
-#perform gradient descent 
+# here x is columns
+X = my_data[:, 0].reshape(-1,1) # -1 tells numpy to figure out the dimension by itself
+ones = np.ones([X.shape[0], 1])
+X = np.concatenate([ones, X],1)
+#X = np.hstack([ones, X]) # hstack Stack arrays in sequence horizontally (column wise)
+
+
+# theta is a row vector
+theta = np.array([[1.0, 1.0]])
+
+# y is a columns vector
+y = my_data[:, 1].reshape(-1,1)
+
 g, cost = gradientDescent(X, y, theta, alpha, iters)  
-print(g)
+print(g, cost)
 
-
-# In[222]:
-
-computeCost(X, y, g)
-
-
-# In[ ]:
-
-
-
+plt.scatter(my_data[:, 0].reshape(-1,1), y)
+axes = plt.gca()
+x_vals = np.array(axes.get_xlim()) 
+y_vals = g[0][0] + g[0][1]* x_vals #the line equation
+plt.plot(x_vals, y_vals, '--')
