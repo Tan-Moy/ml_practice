bde6ffcd2197a7c0636a3c419e0bd2235ab93a29
Tan Moy <tanmoy.pto@gmail.com>
2018-02-21 23:45:51 +0530

    added book repo

--- D3js/project1/index.js
+++ D3js/project1/index.js
@@ -1,59 +1,60 @@
-var dataset = [[5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-[410, 12], [475, 44], [25, 67], [85, 21], [220, 88]];
+//Width and height
+var w = 600;
+var h = 250;
 
-var w = 500;
-var h = 200;
-var p = 20;
+var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+	11, 12, 15, 20, 18, 17, 16, 18, 23, 25
+];
 
-var svgContainer = d3.select('body')
-						.append('svg')
-						.attr('with',w)
-						.attr('height',h);
+var xScale = d3.scaleBand()
+	.domain(d3.range(dataset.length))
+	.rangeRound([0, w])
+	.paddingInner(0.05);
 
-var circlesContainer = svgContainer.selectAll('circle')
-							.data(dataset)
-							.enter()
-							.append('circle')
+var yScale = d3.scaleLinear()
+	.domain([0, d3.max(dataset)])
+	.range([0, h]);
 
-var textContainer = svgContainer.selectAll('text').data(dataset).enter().append("text");
+//Create SVG element
+var svg = d3.select("body")
+	.append("svg")
+	.attr("width", w)
+	.attr("height", h);
 
+//Create bars
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+	.attr("x", function (d, i) {
+		return xScale(i);
+	})
+	.attr("y", function (d) {
+		return h - yScale(d);
+	})
+	.attr("width", xScale.bandwidth())
+	.attr("height", function (d) {
+		return yScale(d);
+	})
+	.attr("fill", function (d) {
+		return "rgb(0, 0, " + Math.round(d * 10) + ")";
+	});
 
-var scaleConfigX = d3.scaleLinear()
-.domain([0,d3.max(dataset,function(d){return d[0]})])
-.range([p, w - p]);
-
-var scaleConfigY = d3.scaleLinear()
-.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-.range([h - p, p]);
-
-
-circlesContainer.attr("cx", function(d) {
-				return scaleConfigX(d[0]);
-				})
-				.attr("cy", function(d) {
-				return scaleConfigY(d[1]);
-				})
-				.attr("r", function(d){
-					return Math.sqrt(h-d[1]);
-				});
-
-textContainer.text(d => d[0] + "," + d[1])
-	.attr("x", d => scaleConfigX(d[0]))
-	.attr("y", d => scaleConfigY(d[1]))
+//Create labels
+svg.selectAll("text")
+	.data(dataset)
+	.enter()
+	.append("text")
+	.text(function (d) {
+		return d;
+	})
+	.attr("text-anchor", "middle")
+	.attr("x", function (d, i) {
+		return xScale(i) + xScale.bandwidth() / 2;
+	})
+	.attr("y", function (d) {
+		return h - yScale(d) + 14;
+	})
 	.attr("font-family", "sans-serif")
 	.attr("font-size", "11px")
-	.attr("fill", "red");
-
-var xAxis = d3.axisBottom(scaleConfigX);
-
-svgContainer.append('g')
-.attr("class", "axis")
-.attr("transform", "translate(0," + (h - p) + ")")
-.call(xAxis)
-
-var yAxis = d3.axisLeft(scaleConfigY).ticks(5);
-
-svgContainer.append('g')
-.attr("class", "axis")
-.attr("transform", "translate(" + p + ",0)")
-.call(yAxis)
+	.attr("fill", "white");
